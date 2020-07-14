2ebfc7c27817b632a3b2d8d931aea6e9a5cf6631
srkucd <srkwhu@gmail.com>
2020-07-14 21:27:01 +0100

    Finish local backup, and ready to save the backup to S3. A big obstacle ahead is how to load the data to Keyspaces.

--- nosql.py
+++ nosql.py
@@ -1,28 +1,34 @@
 from pyspark.sql import SparkSession
 import pandas as pd
+import os
 
+sql = '''SELECT cicid, i94yr, i94mon, i94cit, i94res, i94port,
+       arrdate, i94mode, i94addr, depdate, i94bir, i94visa,
+       count, dtadfile, visapost, occup, entdepa, entdepd,
+       entdepu, matflag, biryear, dtaddto, gender, insnum,
+       airline, admnum, fltno, visatype FROM i94'''
 
-def create_spark():
-    """
-
-    :return:spark session
-    """
-    spark=SparkSession.builder.\
-    config('spark.jars.packages', 'saurfang:spark.sas7bdat:2.0.0-s_2.11').\
+spark = SparkSession.builder. \
+    config('spark.jars.packages', 'saurfang:spark.sas7bdat:2.0.0-s_2.11'). \
     enableHiveSupport().getOrCreate()
 
-    return spark
 
-def read_from_s3(spark,months):
+def backup(years, months):
     """
-    Read data from Amazon S3
+    Create parquet file as local and S3 backup, which can make R & W data easier.
 
-    :param spark: Created spark session
-    :param months: Which month of data do you need?
+    :param years: which year you decide to choose, in our beta version, only 2016 available.
+    :param months: which month you decide to choose
     :return:
     """
-    i94_url = 's3://srk-data-eng-capstone/i94/i94_{month}16_sub.sas7bdat'
-    i94=pd.read_sas(i94_url, 'sas7bdat',encoding="ISO-8859-1").drop_duplicates()
-    i94.to_csv('test.csv')
-    df_spark = spark.read.option('header', 'true').csv('test.csv')
+    i94_url = 's3://srk-data-eng-capstone/i94/i94_{month}{year}_sub.sas7bdat'.format(month=months, year=str(years))
+    csv_filename = '{year}_{month}.csv'.format(year=years, month=months)
+    i94 = pd.read_sas(i94_url, 'sas7bdat',
+                      encoding="ISO-8859-1").drop_duplicates()
+    i94.to_csv(csv_filename)
+    df_spark = spark.read.option('header', 'true').csv(csv_filename)
+    df_spark.createOrReplaceTempView('i94')
+    data = spark.sql(sql)
+    data.write.parquet('{year}_{month}.parquet'.format(year=years, month=months), mode='overwrite')
+    os.remove('{year}_{month}.csv'.format(year=years, month=months))
 
